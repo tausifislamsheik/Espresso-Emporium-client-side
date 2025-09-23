1ea81de602f7d8ae97bdd48f506b5d18970f6447
Tausif Islam Sheik <mr.tausif904@gmail.com>
2025-09-24 02:59:15 +0600

    Delete button added

--- src/components/CoffeeCard.jsx
+++ src/components/CoffeeCard.jsx
@@ -1,9 +1,39 @@
 import { IoMdEye } from "react-icons/io";
 import { HiPencil } from "react-icons/hi2";
 import { MdDelete } from "react-icons/md";
+import Swal from "sweetalert2";
 
 const CoffeeCard = ({ coffee }) => {
-  const { name, price, quantity, photo } = coffee;
+  const { _id, name, price, quantity, photo } = coffee;
+
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:3000/coffees/${_id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            if (data.deletedCount) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your file has been deleted.",
+                icon: "success",
+              });
+            }
+          });
+      }
+    });
+  };
   return (
     <div className="card card-side bg-[#F5F4F1] shadow-sm p-7">
       <figure>
@@ -24,9 +54,18 @@ const CoffeeCard = ({ coffee }) => {
           </p>
         </div>
         <div className="flex flex-col space-y-3">
-          <button className="btn text-2xl px-2 bg-[#D2B48C] text-white rounded-lg"><IoMdEye /></button>
-          <button className="btn text-xl px-2 bg-[#3C393B] text-white rounded-lg"><HiPencil /></button>
-          <button className="btn text-2xl px-2 bg-[#EA4744] text-white rounded-lg"><MdDelete /></button>
+          <button className="btn text-2xl px-2 bg-[#D2B48C] text-white rounded-lg">
+            <IoMdEye />
+          </button>
+          <button className="btn text-xl px-2 bg-[#3C393B] text-white rounded-lg">
+            <HiPencil />
+          </button>
+          <button
+            onClick={() => handleDelete(_id)}
+            className="btn text-2xl px-2 bg-[#EA4744] text-white rounded-lg"
+          >
+            <MdDelete />
+          </button>
         </div>
       </div>
     </div>
