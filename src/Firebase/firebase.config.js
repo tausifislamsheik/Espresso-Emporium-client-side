e5bf64b0360c6311dd81bab9d8367e1ce3aa4636
Tausif Islam Sheik <mr.tausif904@gmail.com>
2025-09-27 00:35:06 +0600

    firebase security fix

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDxWIyd_smCyBILVADJTqt7ZH9gRGO7ZtE",
-  authDomain: "espresso-emporium-a4b34.firebaseapp.com",
-  projectId: "espresso-emporium-a4b34",
-  storageBucket: "espresso-emporium-a4b34.firebasestorage.app",
-  messagingSenderId: "815387434406",
-  appId: "1:815387434406:web:54ccf652cadfe3f41a0367"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
